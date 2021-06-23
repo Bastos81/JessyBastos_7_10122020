3a29f0099c25026ca298c0437962ca07f15801e6
Jessy Bastos <jessybastos.web@gmail.com>
2021-06-23 14:15:18 +0200

    Installation plugin front+router

--- frontend/src/store/index.js
+++ frontend/src/store/index.js
@@ -1,15 +1,14 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
+import posts from './posts'
 
 Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
+    companyName: 'Groupomania'
   },
-  mutations: {
-  },
-  actions: {
-  },
-  modules: {
-  }
+  mutations: {},
+  actions: {},
+  modules: {posts}
 })
